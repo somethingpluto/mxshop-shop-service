0cfd6a38dd23971c290c5d0f77c1b2eba8c78e38
陈同学 <1575197604@qq.com>
2022-08-16 10:57:58 +0800

    fix:category测试

--- goods_service/test/category/main.go
+++ goods_service/test/category/main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"fmt"
 	"github.com/golang/protobuf/ptypes/empty"
+	"goods_service/proto"
 	"goods_service/test"
 )
 
 func main() {
 	test.InitRPCConnect()
-	TestGetAllCategoriesList()
+	//TestGetAllCategoriesList()
+	//TestGetSubCategory()
+	TestCreateCategory()
 }
 
 func TestGetAllCategoriesList() {
@@ -18,4 +21,28 @@ func TestGetAllCategoriesList() {
 		panic(err)
 	}
 	fmt.Println(response.JsonData)
+}
+
+func TestGetSubCategory() {
+	response, err := test.GoodsClient.GetSubCategory(context.Background(), &proto.CategoryListRequest{
+		Id:    130358,
+		Level: 1,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response)
+}
+
+func TestCreateCategory() {
+	response, err := test.GoodsClient.CreateCategory(context.Background(), &proto.CategoryInfoRequest{
+		Name:           "test",
+		ParentCategory: 0,
+		Level:          1,
+		IsTab:          true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response)
 }
